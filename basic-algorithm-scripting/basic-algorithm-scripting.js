b4dd4646a591d149a318d44a5635b04f107e50db
0xluckycoder <lakshanperera.dev@gmail.com>
2023-09-05 21:51:31 +0530

    added truncate string

--- basic-algorithm-scripting/basic-algorithm-scripting.js
+++ basic-algorithm-scripting/basic-algorithm-scripting.js
@@ -140,3 +140,24 @@ function repeatStringNumTimes(str, num) {
     return strArray.join("");
 }
 repeatStringNumTimes("abc", 3);
+
+/**
+ * Truncate a String
+ * Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+ * */
+function truncateString(str, num) {
+  if (str.length <= num) {
+    return str;
+  }
+
+  let newLettersArr = []
+
+  for (let i = 0; i < num; i++) {
+    newLettersArr.push(str[i]);
+  }
+
+  let joinedLetters = `${newLettersArr.join("")}...`;
+  return joinedLetters;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
