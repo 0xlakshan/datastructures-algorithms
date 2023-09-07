206402b69a82ec4905b23e683b6a56521518fbcc
0xluckycoder <lakshanperera.dev@gmail.com>
2023-09-07 12:28:13 +0530

    added Title Case a Sentence

--- basic-algorithm-scripting/basic-algorithm-scripting.js
+++ basic-algorithm-scripting/basic-algorithm-scripting.js
@@ -196,3 +196,33 @@ function booWho(bool) {
 }
 
 booWho(false);
+
+/**
+ * Title Case a Sentence
+ * Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+ * For the purpose of this exercise, you should also capitalize connecting words like the and of.
+ * */ 
+function titleCase(str) {
+  let letterArr = [];
+  let isPreviousItemIsASpace = false;
+  for (let i = 0; i < str.length; i++) {
+    if (i === 0) {
+      letterArr.push(str[i].toUpperCase());
+    } else if (isPreviousItemIsASpace) {
+      letterArr.push(str[i].toUpperCase());
+    } else {
+      letterArr.push(str[i].toLowerCase());
+    }
+
+    if (str[i] === " ") {
+      isPreviousItemIsASpace = true;
+    } else {
+      isPreviousItemIsASpace = false;
+    }
+  }
+
+  let finalSentence = letterArr.join("");
+  return finalSentence;
+}
+
+titleCase("I'm a little tea pot");
