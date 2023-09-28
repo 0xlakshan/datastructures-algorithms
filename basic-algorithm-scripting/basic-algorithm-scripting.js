0b689d0e6ba9f9b13d2117a0173e646d6dd1669c
0xluckycoder <lakshanperera.dev@gmail.com>
2023-09-28 11:04:58 +0530

    added search and replace

--- basic-algorithm-scripting/basic-algorithm-scripting.js
+++ basic-algorithm-scripting/basic-algorithm-scripting.js
@@ -401,4 +401,50 @@ translatePigLatin("consonant");
 translatePigLatin("rhythm");
 translatePigLatin("schwartz");
 translatePigLatin("california");
-translatePigLatin("algorithm")
+translatePigLatin("algorithm");
+
+
+/**
+ * Search and Replace
+ * Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+ * First argument is the sentence to perform the search and replace on.
+ * Second argument is the word that you will be replacing (before).
+ * Third argument is what you will be replacing the second argument with (after).
+ * Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
+*/
+
+function myReplace(str, before, after) {
+  const arrayOfWords = str.split(" ");
+  const newArray = arrayOfWords.map((word) => {
+    if (word === before) {
+      if (word[0] === word[0].toUpperCase()) {
+        let letterArray = after.split("");
+        let convertedFirstLetter = after[0].toUpperCase();
+        letterArray[0] = convertedFirstLetter;
+        let stringConverted = letterArray.join("");
+        return stringConverted;
+      } else if (word[0] === word[0].toLowerCase()) {
+        let letterArray = after.split("");
+        let convertedFirstLetter = after[0].toLowerCase();
+        letterArray[0] = convertedFirstLetter;
+        let stringConverted = letterArray.join("");
+        return stringConverted;
+      } else {
+        return after;
+      }
+    } else {
+      return word;
+    }
+  });
+
+  const stringConvertedWords = newArray.join(" ");
+  console.log(stringConvertedWords);
+  return stringConvertedWords;
+}
+
+myReplace("Let us go to the store", "store", "mall")
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting")
+myReplace("I think we should look up there", "up", "Down");
+myReplace("This has a spellngi error", "spellngi", "spelling")
+myReplace("His name is Tom", "Tom", "john")
+myReplace("Let us get back to more Coding", "Coding", "algorithms")
