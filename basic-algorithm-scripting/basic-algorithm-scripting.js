84fd6beac2df806de4387458599ddf6c58e17ada
Lakshan Perera <39025880+0xluckycoder@users.noreply.github.com>
2025-03-25 23:30:57 +0530

    added Two Sum problem

--- basic-algorithm-scripting/basic-algorithm-scripting.js
+++ basic-algorithm-scripting/basic-algorithm-scripting.js
@@ -490,3 +490,21 @@ pairElement("GCG");
 pairElement("ATCGA");
 pairElement("TTGAG");
 pairElement("CTCTA");
+
+
+
+/*
+Given an array of integers nums and an integer target, return the indices of two numbers that add up to the target. Assume exactly one solution exists, and you may not use the same element twice.
+*/
+
+function twoSum(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+}
